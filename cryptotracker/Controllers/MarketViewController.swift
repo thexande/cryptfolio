b34c1d6c7d3f1f12e67a0af7877ac1d503d1fa04
Alexander Murphy <amurphy345@gmail.com>
2018-07-15 16:35:07 -0600

    add double extension

--- cryptotracker/Controllers/MarketViewController.swift
+++ cryptotracker/Controllers/MarketViewController.swift
@@ -15,15 +15,6 @@ import RealmSwift
 import Hydra
 import Lottie
 
-
-extension Double {
-    /// Rounds the double to decimal places value
-    func rounded(toPlaces places:Int) -> Double {
-        let divisor = pow(10.0, Double(places))
-        return (self * divisor).rounded() / divisor
-    }
-}
-
 class MarketViewController: UIViewController {
     let searchController = UISearchController(searchResultsController: nil)
     fileprivate let searchEmptyStateView = SearchEmptyStateView()
